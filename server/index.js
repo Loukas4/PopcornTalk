dcbe9f212d8d5a1c6b02b6ed22320fa7269ecf3d
dimpapas <dmspaps@gmail.com>
2026-03-04 02:08:06 +0200

    Fix: Correctly import auth routes in index.js

--- server/index.js
+++ server/index.js
@@ -2,14 +2,17 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const bcrypt = require('bcrypt');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Import routes
+const authRoutes = require('./routes/auth');
+
 // Middleware
 app.use(cors());
-app.use(express.json()); // For parsing JSON bodies
+app.use(express.json());    // For parsing JSON bodies
+app.use('/api/auth', authRoutes);// Use auth routes for any requests to /auth
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI)
